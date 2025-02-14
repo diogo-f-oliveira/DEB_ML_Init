f3fe12863d42f72ede16785fa19e32aded56495a
Diogo Oliveira <diogo.oliveira898@gmail.com>
2025-02-14 16:23:37 +0000

    Evaluation metrics now work on parameter scale
    Model predictions are converted to DEB parameter values in functions convert_output_to_parameter_predictions and get_core_parameter_predictions of inference.parameters.py

--- project_code/evaluate/prediction_error.py
+++ project_code/evaluate/prediction_error.py
@@ -4,11 +4,61 @@ import numpy as np
 import sklearn.metrics
 import pandas as pd
 import inspect
-
+import torch
 from sklearn import metrics as metrics
 from tabulate import tabulate
 
-from DEB_ML_Bijection.project_code.evaluate.score import mean_deb_loss
+from .score import mean_deb_loss, geometric_error_factor, symmetric_mean_absolute_percentage_error
+from ..inference.parameters import convert_output_to_parameter_predictions, PARAMETER_COLS
+
+METRIC_LABEL_TO_NAME = {'mean_absolute_percentage_error': 'MAPE',
+                        'symmetric_mean_absolute_percentage_error': 'sMAPE',
+                        'geometric_error_factor': 'GEF',
+                        'mean_deb_loss': 'DEB Loss',
+                        }
+
+
+def compute_parameter_mape(y_true, y_pred, output_col_names):
+    target_df = convert_output_to_parameter_predictions(y_true, output_col_names)
+    pred_df = convert_output_to_parameter_predictions(y_pred, output_col_names)
+
+    mape_df = compute_metrics(
+        y_true=target_df,
+        y_pred=pred_df,
+        output_col_names=PARAMETER_COLS,
+        metrics=['mean_absolute_percentage_error'],
+    )
+
+    return mape_df
+
+
+def evaluate_parameter_predictions_on_data(data, col_types, model, print_score=False, save_score=False,
+                                           results_save_path=None):
+    n_outputs = len(col_types['output']['all'])
+    output_col_names = col_types['output']['all']
+    y_true = data['output'].reshape(-1, n_outputs)
+    y_pred = model.predict(data['input']).reshape(-1, n_outputs)
+    target_df = convert_output_to_parameter_predictions(y_true, output_col_names)
+    pred_df = convert_output_to_parameter_predictions(y_pred, output_col_names)
+
+    metrics_df = compute_metrics(
+        y_true=target_df.values,
+        y_pred=pred_df.values,
+        output_col_names=PARAMETER_COLS,
+        metrics=['mean_absolute_percentage_error',
+                 geometric_error_factor,
+                 symmetric_mean_absolute_percentage_error,
+                 mean_deb_loss,
+                 ]
+    )
+    if print_score:
+        print(f"GEF: {metrics_df['geometric_error_factor'].mean():.4f}")
+        print(tabulate(metrics_df, headers='keys', tablefmt='simple'))
+
+    if save_score:
+        metrics_df.to_csv(results_save_path)
+
+    return metrics_df
 
 
 def compute_metrics(y_true, y_pred, metrics=None, output_col_names=None):
@@ -37,17 +87,6 @@ def compute_metrics(y_true, y_pred, metrics=None, output_col_names=None):
                 metric_values[i] = metric(y_true[:, i], y_pred[:, i])
         metrics_dict[metric_name] = {p: e for p, e in zip(output_col_names, metric_values)}
 
-    # for m in parameter_metrics:
-    #     metric = getattr(sklearn.metrics, m)
-    #     signature = inspect.signature(metric)
-    #     if 'multioutput' in signature.parameters:
-    #         metric_values = metric(y_true_unscaled, y_pred_unscaled, multioutput='raw_values')
-    #     else:
-    #         metric_values = np.zeros(shape=(len(output_col_names),))
-    #         for i in range(len(output_col_names)):
-    #             metric_values[i] = metric(y_true_unscaled[:, i], y_pred_unscaled[:, i])
-    #     metrics_dict[m] = {p: e for p, e in zip(output_col_names, metric_values)}
-
     # Pack into DataFrame
     metrics_df = pd.DataFrame.from_dict(metrics_dict, orient='index').transpose()
 
@@ -55,12 +94,12 @@ def compute_metrics(y_true, y_pred, metrics=None, output_col_names=None):
 
 
 def evaluate_on_data(data, col_types, model, print_score=False, save_score=False, results_save_path=None):
-    y_pred_test = model.predict(data['input']).reshape(-1, 1)
-    y_pred_unscaled_test = model.output_transformer.inverse_transform(y_pred_test).reshape(-1, 1)
-    y_true_test = model.output_transformer.transform(data['output']).reshape(-1, 1)
-    y_true_unscaled_test = data['output'].reshape(-1, 1)
+    n_outputs = len(col_types['output']['all'])
+    y_pred_test = model.predict(data['input']).reshape(-1, n_outputs)
+    y_pred_unscaled_test = model.output_transformer.inverse_transform(y_pred_test).reshape(-1, n_outputs)
+    y_true_test = model.output_transformer.transform(data['output']).reshape(-1, n_outputs)
+    y_true_unscaled_test = data['output'].reshape(-1, n_outputs)
 
-    r2_test_score = model.score(data['input'], data['output'])
     output_metrics_df = compute_metrics(
         y_true=y_true_test,
         y_pred=y_pred_test,
@@ -69,6 +108,8 @@ def evaluate_on_data(data, col_types, model, print_score=False, save_score=False
                  'max_error'],
         output_col_names=col_types['output']['all'],
     )
+    r2_test_score = output_metrics_df['r2_score'].mean()
+
     unscaled_metrics_df = compute_metrics(
         y_true=y_true_unscaled_test,
         y_pred=y_pred_unscaled_test,
@@ -82,7 +123,6 @@ def evaluate_on_data(data, col_types, model, print_score=False, save_score=False
     metrics_df = pd.concat([output_metrics_df, unscaled_metrics_df], axis=1)
 
     if print_score:
-        print("Test set")
         print(f"R^2 score: {r2_test_score:.4f}")
         print(tabulate(metrics_df, headers='keys', tablefmt='simple'))
 
@@ -90,3 +130,13 @@ def evaluate_on_data(data, col_types, model, print_score=False, save_score=False
         # test_performance_save_folder = os.path.join(save_folder, 'test_performance')
         # metrics_df.to_csv(os.path.join(test_performance_save_folder, f"{best_model_name}.csv"))
         metrics_df.to_csv(results_save_path)
+
+
+def evaluate_pytorch_model(dataloader, model, criterion):
+    eval_loss = 0
+    model.eval()
+    with torch.no_grad():
+        for X_batch, y_batch in dataloader:
+            outputs = model(X_batch)
+            eval_loss += criterion(outputs, y_batch).item()
+        return eval_loss / len(dataloader)
--- project_code/inference/parameters.py
+++ project_code/inference/parameters.py
@@ -1,34 +1,92 @@
+import numpy as np
 import pandas as pd
 
 from ..algorithms.theoretical import predict_E_G_from_theory
 
-CORE_DEB_PARS = ['z', 'p_M', 'kap', 'v', 'E_G', 'h_a', 'E_Hb', 'E_Hp']
+PARAMETER_COLS = ['p_Am', 'kap', 'v', 'p_M', 'E_Hb', 'E_Hp', 'k_J', 'E_Hj', 'E_Hx', 's_M']
+AmP_CORE_DEB_PARS = ['z', 'p_M', 'kap', 'v', 'E_G', 'h_a', 'E_Hb', 'E_Hx', 'E_Hj', 'E_Hp', 'k_J']
+DEFAULT_VALUES = {
+    'h_a': 3.0e-9,
+    'kap_G': 0.8,
+    'mu_V': 500_000,
+    'w_V': 23.9,
+    'k_J': 0.002,
+    's_M': 1,
+}
+UPPER_BOUNDS = {
+    'kap': 1,
+    '1-kap': 1,
+    's_p': 0.8 ** 3,
+    's_H': 1,
+    's_M': 1,
+}
+
+
+def convert_output_to_parameter_predictions(y, output_col_names):
+    pars_df = pd.DataFrame(y, columns=output_col_names)
+
+    # If the model did not predict 'k_J', set it to default value
+    if 'k_J' not in pars_df.columns:
+        pars_df['k_J'] = DEFAULT_VALUES['k_J']
+
+    # If the model did not predict 's_M', set it to default value
+    if 's_M' not in pars_df.columns:
+        pars_df['s_M'] = DEFAULT_VALUES['s_M']
+
+    # If the model predicts '1-kap' instead of 'kap', compute 'kap'
+    if 'kap' not in pars_df.columns and '1-kap' in pars_df.columns:
+        pars_df['kap'] = 1 - pars_df['1-kap']
+        pars_df.drop(columns=['1-kap'], inplace=True)
+
+    # If the model predicts 's_H' instead of 'E_Hb', compute 'E_Hb'
+    if 'E_Hb' not in pars_df.columns and {'s_H', 'E_Hp'}.issubset(pars_df.columns):
+        pars_df['E_Hb'] = pars_df['s_H'] * pars_df['E_Hp']
+        pars_df.drop(columns=['s_H'], inplace=True)
+
+    # If the model predicts 's_p' instead of 'p_M', compute 'p_M'
+    if 'p_M' not in pars_df.columns:
+        if {'s_p', 'kap', 'E_Hp', 'p_Am'}.issubset(pars_df.columns):
+            pars_df['p_M'] = np.sqrt(np.power(pars_df['s_p'] * pars_df['kap'], 2) * (1 - pars_df['kap']) *
+                                     np.power(pars_df['p_Am'], 3) / pars_df['k_J'] / pars_df['E_Hp'])
+            pars_df.drop(columns=['s_p'], inplace=True)
+
+    # If the model did not predict 'E_Hj', set it to 1.05x 'E_Hb'
+    if 'E_Hx' not in pars_df.columns and 'E_Hb' in pars_df.columns:
+        pars_df['E_Hx'] = 1.05 * pars_df['E_Hb']
+
+    # If the model did not predict 'E_Hj', set it to 1.05x 'E_Hb'
+    if 'E_Hj' not in pars_df.columns and 'E_Hb' in pars_df.columns:
+        pars_df['E_Hj'] = 1.05 * pars_df['E_Hb']
+
+    pars_df = pars_df[PARAMETER_COLS].copy()
+
+    return pars_df
 
 
 def get_core_parameter_predictions(dfs, pred_df):
-    pars_df = pd.DataFrame(index=pred_df.index, columns=['data_split'] + CORE_DEB_PARS, dtype='float')
-    pars_df['data_split'] = pred_df['data_split']
+    # TODO: Call convert_output_to_parameter_predictions
+    pars_df = pred_df.copy()
     pars_df.index.name = 'species'
 
-    # If the model predicted a core parameter, copy the predictions
-    for p in CORE_DEB_PARS:
-        if p in pred_df.columns:
-            pars_df[p] = pred_df[p]
+    pars_df = convert_output_to_parameter_predictions(pars_df, pars_df.columns)
 
-    # If the model predicted the ratio 'E_Hb/E_Hp' instead of 'E_Hb', compute 'E_Hb'
-    if 'E_Hb' not in pred_df.columns and 'E_Hb_p' in pred_df.columns and 'E_Hp' in pred_df.columns:
-        pars_df['E_Hb'] = pred_df['E_Hb_p'] * pred_df['E_Hp']
-
-    # If the model predicted 'p_Am' instead of 'z', compute 'z'
-    if 'z' not in pred_df.columns and 'p_Am' in pred_df.columns and 'p_M' in pred_df.columns and 'kap' in pred_df.columns:
-        pars_df['z'] = pred_df['p_Am'] * pred_df['kap'] / pred_df['p_M']
+    # If the model predicts 'p_Am' instead of 'z', compute 'z'
+    if 'z' not in pars_df.columns:
+        if {'p_Am', 'p_M', 'kap'}.issubset(pars_df.columns):
+            pars_df['z'] = pars_df['p_Am'] * pars_df['kap'] / pars_df['p_M']
 
     # If the model did not predict 'E_G', compute it from theory
-    if 'E_G' not in pred_df.columns:
-        for species in pred_df.index.values:
+    if 'E_G' not in pars_df.columns:
+        for species in pars_df.index.values:
             pars_df.loc[species, 'E_G'] = predict_E_G_from_theory(
-                dfs[pred_df.loc[species, 'data_split']].loc[species, 'd_V'])
+                d_V=dfs[pars_df.loc[species, 'data_split']].loc[species, 'd_V'],
+                kap_G=DEFAULT_VALUES['kap_G'],
+                mu_V=DEFAULT_VALUES['mu_V'],
+                w_V=DEFAULT_VALUES['w_V'],
+            )
 
-    pars_df[CORE_DEB_PARS] = pars_df[CORE_DEB_PARS].astype('float')
+    # Convert to float
+    pars_df[AmP_CORE_DEB_PARS] = pars_df[AmP_CORE_DEB_PARS].astype('float')
 
-    return pars_df
+    # Return only the predicitons for core parameters
+    return pars_df[['data_split'] + AmP_CORE_DEB_PARS].copy()
