a507d8a6369251bbd0ae6b756144a1ebf062cf92
Diogo Oliveira <diogo.oliveira898@gmail.com>
2025-02-18 16:59:42 +0000

    Infeasibility Loss for Neural Networks
    New class MSEInfeasibilityLoss: implements constraints on parameter values as soft constraints in the loss function
    New class MaskedMSELoss: MSE Loss with mask for outputs that are DEB model dependent

--- project_code/algorithms/loss_functions.py
+++ project_code/algorithms/loss_functions.py
@@ -1,16 +1,100 @@
+import numpy as np
 import torch
 import torch.nn as nn
 
+from ..inference.parameters import DEFAULT_VALUES, UPPER_BOUNDS
+from ..data.prepare_data_pytorch import DEFAULT_TORCH_DTYPE
 
-class MSEInfeasibiltyLoss(nn.Module):
-    w_V = 23.9
-    mu_V = 550_000
-    k_J = 0.002
 
-    def __init__(self, scalers, col_types, lambdas=None):
-        super(MSEInfeasibiltyLoss, self).__init__()
-        self.mse_loss = nn.MSELoss()
+class MaskedMSELoss(nn.MSELoss):
+    def __init__(self, size_average=None, reduce=None, reduction='mean'):
+        super(MaskedMSELoss, self).__init__(size_average, reduce, reduction)
+        self.size_average = size_average
+        self.reduce = reduce
+        self.reduction = reduction
 
+    def forward(self, predictions, targets, mask):
+        # Calculate element-wise squared errors
+        errors = (predictions - targets) ** 2
+
+        # Apply mask (outputs with mask=0 are excluded from loss)
+        masked_errors = errors * mask
+
+        # Calculate mean squared error manually (ignoring masked outputs)
+        loss = masked_errors.sum() / mask.sum()
+
+        return loss
+
+
+class MSEInfeasibilityLoss(nn.Module):
+    def __init__(self, col_types, output_transformer, lambdas=None):
+        super(MSEInfeasibilityLoss, self).__init__()
+        self.criterion = MaskedMSELoss()
+
+        self.output_transformer = output_transformer
+
+        # Compute upper bounds in model scale
+        upper_bounds = torch.zeros(3, dtype=DEFAULT_TORCH_DTYPE)
+        bounded_col_idx = torch.zeros(3, dtype=torch.long)
+        for i, col in enumerate(['1-kap', 's_p_M', 's_Hb_bj', 's_Hbj_p']):
+            idx = col_types['output']['all'].index(col)
+            bounded_col_idx[i] = idx
+            upper_bound_par_scale = UPPER_BOUNDS[col_types['output']['all'][idx]]
+            mean = self.output_transformer.mean[idx]
+            std = self.output_transformer.std[idx]
+            upper_bounds[i] = (np.log(upper_bound_par_scale) - mean) / std
+
+        # Register as buffers so they can be moved to GPU
+        self.register_buffer("upper_bounds", upper_bounds)
+        self.register_buffer("bounded_col_idx", bounded_col_idx)
+
+        if lambdas is None:
+            lambdas = torch.ones([4], dtype=DEFAULT_TORCH_DTYPE)
+        self.register_buffer("lambdas", lambdas)
+        self.loss_values = torch.zeros(4, dtype=DEFAULT_TORCH_DTYPE)
+
+    def forward(self, outputs, targets, mask):
+        mse_loss = self.criterion(outputs, targets, mask)
+
+        # kap must be smaller than 1
+        kappa_loss = self.kappa_loss(outputs)
+        # Must reach puberty
+        reach_pub_loss = self.reach_puberty_loss(outputs)
+        # Maturity levels must increase
+        mat_levels_loss = self.maturity_levels_loss(outputs)
+
+        self.loss_values = torch.stack([mse_loss, kappa_loss, reach_pub_loss, mat_levels_loss])
+
+        return torch.dot(self.loss_values, self.lambdas)
+
+    def kappa_loss(self, outputs):
+        kap_idx = self.bounded_col_idx[0]
+        upper_bound = self.upper_bounds[0]
+        return torch.mean(torch.square(torch.relu(outputs[:, kap_idx] - upper_bound)))
+
+    def reach_puberty_loss(self, outputs):
+        s_p_idx = self.bounded_col_idx[1]
+        upper_bound = self.upper_bounds[1]
+        return torch.mean(torch.square(torch.relu(outputs[:, s_p_idx] - upper_bound)))
+
+    def maturity_levels_loss(self, outputs, mask):
+        # Penalty for E_Hj > E_Hb
+        idx_s_Hb_bj = self.bounded_col_idx[2]
+        ub_s_Hb_bj = self.upper_bounds[2]
+        penalty_s_Hb_bj = torch.square(torch.relu(outputs[:, idx_s_Hb_bj] - ub_s_Hb_bj))
+
+        # Penalty for E_Hj > E_Hb
+        idx_s_Hbj_p = self.bounded_col_idx[3]
+        ub_s_Hbj_p = self.upper_bounds[3]
+        penalty_s_Hbj_p = torch.square(torch.relu(outputs[:, idx_s_Hbj_p] - ub_s_Hbj_p))
+
+        return torch.mean(penalty_s_Hb_bj + penalty_s_Hbj_p)
+
+
+class MSEInfeasibiltyLossParameterScale(nn.Module):
+
+    def __init__(self, scalers, col_types, lambdas=None, output_weights=None):
+        super(MSEInfeasibiltyLossParameterScale, self).__init__()
         self.scalers = scalers
 
         input_cols = col_types['input']['all']
@@ -18,66 +102,67 @@ class MSEInfeasibiltyLoss(nn.Module):
         self.input_col_idx = {col: i for i, col in enumerate(input_cols)}
         self.output_col_idx = {col: i for i, col in enumerate(output_cols)}
 
+        if output_weights is None:
+            output_weights = torch.ones(len(output_cols))
+        self.output_weights = output_weights
+        self.loss_values = None
+
         if lambdas is None:
             self.lambdas = torch.ones([4], dtype=torch.float32)
         else:
             self.lambdas = lambdas
 
-    def forward(self, outputs, targets, inputs):
+    def weighted_mse_loss(self, y_pred, y_true):
+        return torch.mean(self.output_weights * (y_true - y_pred) ** 2)
+
+    def forward(self, outputs, targets):
         # Standard MSE loss
-        mse_loss = self.mse_loss(outputs, targets)
+        weighted_mse_loss = self.weighted_mse_loss(outputs, targets)
 
         # Unscale inputs and outputs
-        inputs_unscaled = self.scalers['input'].inverse_transform(inputs)
+        # inputs_unscaled = self.scalers['input'].inverse_transform(inputs)
         outputs_unscaled = self.scalers['output'].inverse_transform(outputs)
 
         # Maturity levels must increase
         mat_level_penalty = self.maturity_levels_loss(outputs_unscaled)
-        # Growth efficiency kap_G must be lower than 1
-        kap_G_penalty = self.growth_efficiency_loss(inputs_unscaled, outputs_unscaled)
+        # Efficiency must be smaller than 1
+        efficiency_loss = self.efficiency_loss(outputs_unscaled)
         # Must reach puberty
-        reach_pub_loss = self.reach_puberty_loss(inputs_unscaled, outputs_unscaled)
+        reach_pub_loss = self.reach_puberty_loss(outputs_unscaled)
 
-        losses = torch.stack([mse_loss, mat_level_penalty, kap_G_penalty, reach_pub_loss])
-        # losses = torch.stack([mse_loss, mat_level_penalty, kap_G_penalty])
+        self.loss_values = torch.stack([weighted_mse_loss, mat_level_penalty, efficiency_loss, reach_pub_loss])
 
-        return torch.dot(losses, self.lambdas)
+        return torch.dot(self.loss_values, self.lambdas)
 
     def maturity_levels_loss(self, outputs_unscaled):
-        b_p_mat_ratio = outputs_unscaled[:, self.output_col_idx['E_Hb']] / outputs_unscaled[:,
-                                                                           self.output_col_idx['E_Hp']]
-        b_bjx_mat_ratio = outputs_unscaled[:, self.output_col_idx['E_Hb']] / outputs_unscaled[:,
-                                                                             self.output_col_idx['E_Hbjx']]
-        bjx_p_mat_ratio = outputs_unscaled[:, self.output_col_idx['E_Hp']] / outputs_unscaled[:,
-                                                                             self.output_col_idx['E_Hbjx']]
+        if 'E_Hb_p' in self.output_col_idx:
+            b_p_mat_ratio = outputs_unscaled[:, self.output_col_idx['E_Hb_p']]
+        elif 'E_Hb' in self.output_col_idx and 'E_Hp' in self.output_col_idx:
+            E_Hb = outputs_unscaled[:, self.output_col_idx['E_Hb']]
+            E_Hp = outputs_unscaled[:, self.output_col_idx['E_Hp']]
+            b_p_mat_ratio = E_Hb / E_Hp
 
-        b_p_loss = torch.sum(torch.relu(b_p_mat_ratio - 1))
-        b_bjx_loss = torch.sum(torch.relu(b_bjx_mat_ratio - 1))
-        bjx_p_loss = torch.sum(torch.relu(bjx_p_mat_ratio - 1))
+        b_p_loss = torch.sum(torch.relu(b_p_mat_ratio - 1) ** 2)
 
-        return b_p_loss + b_bjx_loss + bjx_p_loss
+        return b_p_loss
 
-    def growth_efficiency_loss(self, inputs_unscaled, outputs_unscaled):
-        # Assume standard composition
-        M_V = inputs_unscaled[:, self.input_col_idx['d_V']] / self.w_V
-        kap_G = self.mu_V * M_V / outputs_unscaled[:, self.output_col_idx['E_G']]
+    def efficiency_loss(self, outputs_unscaled):
+        if 'kap' in self.output_col_idx:
+            kap = outputs_unscaled[:, self.output_col_idx['kap']]
+        elif '1-kap' in self.output_col_idx:
+            kap = 1 - outputs_unscaled[:, self.output_col_idx['1-kap']]
+        return torch.sum(torch.relu(kap - 1) ** 2)
 
-        return torch.sum(torch.relu(kap_G - 1))
+    def reach_puberty_loss(self, outputs_unscaled):
+        if 'kap' in self.output_col_idx:
+            kap = outputs_unscaled[:, self.output_col_idx['kap']]
+        elif '1-kap' in self.output_col_idx:
+            kap = 1 - outputs_unscaled[:, self.output_col_idx['1-kap']]
 
-    def reach_puberty_loss(self, inputs, outputs_unscaled):
-        s_M_cubed = (1 + (inputs[:, self.input_col_idx['Wwbjx']] / inputs[:, self.input_col_idx['Wwb']] - 1) *
-                     inputs[:, self.input_col_idx['metamorphosis']])
-        # with torch.no_grad():
-        #     b_idx = self.input_col_idx['Wwb']
-        #     bjx_idx = self.input_col_idx['Wwbjx']
-        #     log_Wwbjx = inputs[:, bjx_idx] * self.input_scaler.std[bjx_idx] + self.input_scaler.mean[bjx_idx]
-        #     log_Wwb = inputs[:, b_idx] * self.input_scaler.std[b_idx] + self.input_scaler.mean[b_idx]
-        #     s_M_cubed = torch.exp(log_Wwbjx - log_Wwb)
-        kap = outputs_unscaled[:, self.output_col_idx['kap']]
         p_Am = outputs_unscaled[:, self.output_col_idx['p_Am']]
         p_M = outputs_unscaled[:, self.output_col_idx['p_M']]
         E_Hp = outputs_unscaled[:, self.output_col_idx['E_Hp']]
-        left_hand_side = self.k_J * E_Hp * torch.pow(p_M, 2) / (
-                (1 - kap + 1e-10) * torch.pow(kap, 2) * torch.pow(p_Am, 3) * s_M_cubed)
+        left_hand_side = DEFAULT_VALUES['k_J'] * E_Hp * torch.pow(p_M, 2) / (
+                (1 - kap + 1e-10) * torch.pow(kap, 2) * torch.pow(p_Am, 3))
 
-        return torch.sum(torch.relu(left_hand_side - 1))
+        return torch.sum(torch.relu(left_hand_side - 1) ** 2)
