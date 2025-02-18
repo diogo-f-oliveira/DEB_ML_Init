47c438709ddd39fd426df7881f1d51623c9c5c8d
Diogo Oliveira <diogo.oliveira898@gmail.com>
2025-02-18 16:42:54 +0000

    Scikit-learn models now always predict on parameter scale

--- project_code/algorithms/build_sklearn_model.py
+++ project_code/algorithms/build_sklearn_model.py
@@ -4,44 +4,46 @@ from sklearn.multioutput import MultiOutputRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer
 
-from ..data.prepare_data_sklearn import LogScaleTransformer
+from ..data.prepare_data_sklearn import LogScaleClipTransformer
 
 
 class RegressorOnTransformedInput(RegressorMixin, BaseEstimator):
     def __init__(self, regressor, input_scaler_type, col_types):
-        self.base_regressor = regressor
+        self.regressor = regressor
         self.regressor_ = None
         self.input_scaler_type = input_scaler_type
-        self.input_transformer = LogScaleTransformer(
+        self.input_transformer = LogScaleClipTransformer(
             all_col_names=col_types['input']['all'],
             log_col_names=col_types['input']['log'],
+            clip_col_names=col_types['input']['bounded01'],
             scale_col_names=col_types['input']['scale'],
             scaler_type=input_scaler_type
         )
         self.col_types = col_types
 
     def fit(self, X, y, sample_weight=None):
-        X_transformed = self.input_transformer.fit_transform(X)
+        X_model_scale = self.input_transformer.fit_transform(X)
 
-        self.regressor_ = clone(self.base_regressor)
+        self.regressor_ = clone(self.regressor)
 
-        self.regressor_.fit(X_transformed, y)
+        self.regressor_.fit(X_model_scale, y)
 
         return self
 
     def predict(self, X, y=None):
-        X_transformed = self.input_transformer.transform(X)
+        X_model_scale = self.input_transformer.transform(X)
 
-        return self.regressor_.predict(X_transformed)
+        return self.regressor_.predict(X_model_scale)
 
 
 class RegressorOnTransformedTarget(RegressorOnTransformedInput):
     def __init__(self, regressor, input_scaler_type, output_scaler_type, col_types):
         super(RegressorOnTransformedTarget, self).__init__(regressor=regressor, input_scaler_type=input_scaler_type,
                                                            col_types=col_types)
-        self.output_transformer = LogScaleTransformer(
+        self.output_transformer = LogScaleClipTransformer(
             all_col_names=col_types['output']['all'],
             log_col_names=col_types['output']['log'],
+            clip_col_names=col_types['output']['bounded01'],
             scale_col_names=col_types['output']['scale'],
             scaler_type=output_scaler_type,
         )
@@ -49,44 +51,31 @@ class RegressorOnTransformedTarget(RegressorOnTransformedInput):
 
     def fit(self, X, y, sample_weight=None):
         # Scale the target
-        y_transformed = self.output_transformer.fit_transform(y)
+        y_model_scale = self.output_transformer.fit_transform(y)
 
         # Fit the model on the scaled target
-        super(RegressorOnTransformedTarget, self).fit(X, y_transformed)
+        # Inputs are scaled as well inside the RegressorOnTransformedInput.fit() method
+        super(RegressorOnTransformedTarget, self).fit(X, y_model_scale)
         return self
+
+    def predict(self, X, y=None):
+        # Predict the output using the scaled input (scaled inside the RegressorOnTransformedInput.predict() method)
+        y_pred_model_scale = super(RegressorOnTransformedTarget, self).predict(X)
+
+        # Predict in parameter scale
+        y_pred_par_scale = self.output_transformer.inverse_transform(y_pred_model_scale)
+
+        return y_pred_par_scale
 
     def score(self, X, y, sample_weight=None):
         # Scale the target for scoring
-        y_scaled = self.output_transformer.transform(y)
+        y_transformed = self.output_transformer.transform(y)
 
         # Score the model on the scaled target
-        return super(RegressorOnTransformedTarget, self).score(X, y_scaled)
-
-
-class BoundedRegressor(RegressorOnTransformedInput):
-    def __init__(self, regressor, input_scaler_type, col_types, a=0, b=1, tol=1e-16):
-        super(BoundedRegressor, self).__init__(regressor=regressor, input_scaler_type=input_scaler_type,
-                                               col_types=col_types)
-        self.a = a
-        self.b = b
-        self.tol = tol
-        # Output transformer is just an identity function to ensure functionality with RegressorOnTransformedTarget
-        self.output_transformer = FunctionTransformer(func=None, inverse_func=None)
-
-    def bound_output(self, y):
-        return np.minimum(self.b - self.tol, np.maximum(self.a + self.tol, y))
-
-    def predict(self, X):
-        # Predict using the fitted model
-        y_pred = super(BoundedRegressor, self).predict(X)
-
-        # Bound the output between a b
-        return self.bound_output(y_pred)
+        return super(RegressorOnTransformedTarget, self).score(X, y_transformed)
 
 
 def build_sklearn_model(regressor, col_types, input_scaler_type='standard', output_scaler_type='standard'):
-    if len(col_types['output']['bounded01']):
-        return BoundedRegressor(regressor=regressor, input_scaler_type=input_scaler_type, col_types=col_types)
-    else:
-        return RegressorOnTransformedTarget(regressor=regressor, input_scaler_type=input_scaler_type, col_types=col_types,
-                                            output_scaler_type=output_scaler_type)
+    return RegressorOnTransformedTarget(regressor=regressor, input_scaler_type=input_scaler_type,
+                                        col_types=col_types,
+                                        output_scaler_type=output_scaler_type)
