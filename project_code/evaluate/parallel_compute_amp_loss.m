23155590aaec609e2582f310549d4e115b7d7d88
Diogo Oliveira <diogo.oliveira898@gmail.com>
2025-03-05 11:55:04 +0000

    MATLAB script to get ground truth parameters and loss values
    - parameters and loss values for estimations in the Add-my-Pet collection

---
+++ project_code/evaluate/parallel_compute_amp_loss.m
@@ -0,0 +1,186 @@
+clear all
+format long
+
+%% Define folders and files
+
+filePathsCSVPath = '..\..\filepaths.csv';
+pathsTable = readtable(filePathsCSVPath, 'Delimiter', ',', 'ReadVariableNames', true, 'ReadRowNames', true);
+allSpeciesFolder = pathsTable{'species_folder', 'path'}{:};
+datasetsFolder = '..\..\data\processed\';
+resultsFolder = '..\..\results\';
+datasetName = 'biologist_no_pub_age';
+modelName = 'AmP';
+
+datasetFile = [datasetsFolder datasetName '\' datasetName '.csv'];
+parameterTableOutputFile = [resultsFolder 'parameter_predictions\' modelName '_predictions.csv'];
+lossTableOutputFile = [resultsFolder 'deb_model_loss\' modelName '_deb_model_loss.csv'];
+
+
+%% Initialize tables
+datasetTable = readtable(datasetFile, 'ReadRowNames', true);
+speciesList = datasetTable.Properties.RowNames;
+numSpecies = length(speciesList);
+
+% Parameter table
+parameterTableCols = {
+    'data_split', 'z', 'p_M', 'kap', 'v', 'E_G', 'h_a', 'E_Hb', 'E_Hj', 'E_Hx', 'E_Hp', 'k_J', ...
+};
+varTypesParameterTable = {
+    'string', 'double', 'double', 'double', 'double', 'double', 'double', 'double', 'double', 'double', 'double', 'double', ... % parameterCols
+    };
+parameterTable = table('Size', [numSpecies, length(parameterTableCols)], 'VariableTypes', varTypesParameterTable, 'VariableNames', parameterTableCols, 'RowNames', speciesList);
+parameterTable{:, 'data_split'} = datasetTable{:, 'data_split'};
+
+% DEB Loss table
+lossTableCols = {'data_split', 'deb_loss', 'success', 'error_type', 'error_message'};
+varTypesLossTable = {'string', 'double', 'logical', 'string', 'string'};
+lossTable = table('Size', [numSpecies, length(lossTableCols)], 'VariableTypes', varTypesLossTable, 'VariableNames', lossTableCols, 'RowNames', speciesList);
+% debLossTable{:, 'data_split'} = parameterEstimates{:, 'data_split'};
+lossTable{:, 'data_split'} = "test";
+
+%% Set up parallel pool
+pool = gcp('nocreate');
+if isempty(pool)
+    pool = parpool('Processes');
+end
+numWorkers = pool.NumWorkers;
+
+% Max execution time per future
+maxTime = 5*60; % in seconds
+includePseudoData = true;
+
+% Initialize variables
+i = 1; % Index of species to submit
+inProgressFutures = struct('future', {}, 'i', {}, 'speciesName', {}, 'startTime', {});
+
+%% Start the processing loop
+while i <= numSpecies || ~isempty(inProgressFutures)
+    % Submit new tasks if workers are available
+    while length(inProgressFutures) < numWorkers && i <= numSpecies
+        speciesName = speciesList{i};
+
+        % Submit parfeval task
+        fut = parfeval(pool, @processSpecies, 4, speciesName, allSpeciesFolder, includePseudoData);
+        % Record the future, species name, start time
+        startTime = tic;
+        nFutures = length(inProgressFutures);
+        inProgressFutures(nFutures+1).future = fut;
+        inProgressFutures(nFutures+1).i = i;
+        inProgressFutures(nFutures+1).speciesName = speciesName;
+        inProgressFutures(nFutures+1).startTime = startTime;
+        % inProgressFutures(end+1) = struct('future', fut, 'speciesName', speciesName, 'startTime', startTime);
+        fprintf('[%4d / %d | %50s] SUBMIT \n', i, numSpecies, speciesName)
+
+        % fprintf('Submitted species %s (%d/%d)\n', speciesName, i, numSpecies);
+        i = i + 1;
+    end
+
+    % Check futures for completion or timeout
+    idx = 1;
+    while idx <= length(inProgressFutures)
+        futInfo = inProgressFutures(idx);
+        if strcmp(futInfo.future.State, 'finished')
+            % Fetch outputs
+            try
+                [par, debLoss, success, error_type] = fetchOutputs(futInfo.future);
+                fprintf('[%4d / %d | %50s] RESULT: %d %s \n', futInfo.i, numSpecies, futInfo.speciesName, success, error_type)
+
+                % Store results
+                lossTable{futInfo.speciesName, 'deb_loss'} = debLoss;
+                lossTable{futInfo.speciesName, 'success'} = success;
+                lossTable{futInfo.speciesName, 'error_type'} = error_type;
+
+                % Get parameter values
+                for p=1:length(parameterTableCols)
+                    parName = parameterTableCols{p};
+                    if isfield(par, parName)
+                        parameterTable{futInfo.speciesName, parName} = par.(parName);
+                    end
+                end
+
+            catch ME
+                % Handle error
+                if isempty(futInfo.future.Error)
+                    error_message = ME.message;
+                else
+                    error_message = futInfo.future.Error.message;
+                end
+                fprintf('[%4d / %d | %50s] ERROR: %s \n', futInfo.i, numSpecies, futInfo.speciesName, error_message)
+                lossTable{futInfo.speciesName, 'deb_loss'} = nan;
+                lossTable{futInfo.speciesName, 'success'} = false;
+                lossTable{futInfo.speciesName, 'error_type'} = "predict_or_loss_error";
+                lossTable{futInfo.speciesName, 'error_message'} = string(error_message);
+            end
+            % Remove future from in-progress list
+            inProgressFutures(idx) = [];
+        else
+            % Check for timeout
+            elapsedTime = toc(futInfo.startTime);
+            if elapsedTime > maxTime
+                cancel(futInfo.future);
+                fprintf('[%4d / %d | %50s] TIMEOUT: predict function took longer than %d seconds to execute. \n', futInfo.i, numSpecies, futInfo.speciesName, maxTime)
+                lossTable{futInfo.speciesName, 'deb_loss'} = nan;
+                lossTable{futInfo.speciesName, 'success'} = false;
+                lossTable{futInfo.speciesName, 'error_type'} = "timeout";
+                % Remove future from in-progress list
+                inProgressFutures(idx) = [];
+            else
+                idx = idx + 1;
+            end
+        end
+    end
+
+    % Optionally, pause for a short time to avoid busy waiting
+    pause(0.1);
+end
+
+%% Write results to a .csv file
+writetable(lossTable, lossTableOutputFile,'WriteRowNames',true);
+fprintf('Loss table saved in %s\n', lossTableOutputFile);
+writetable(parameterTable, parameterTableOutputFile, 'WriteRowNames',true);
+fprintf('Parameter table saved in %s\n', parameterTableOutputFile);
+
+
+%% Function to process each species
+function [par, debLoss, success, errorType] = processSpecies(speciesName, allSpeciesFolder, includePseudoData)
+% Set up data for the species
+speciesFolder = fullfile(allSpeciesFolder, speciesName);
+% Check if the species folder exists
+if isfolder(speciesFolder)
+    % Change directory to the species folder
+    cd(speciesFolder);
+
+    % Run mydata.m
+    [data, auxData, metaData, ~, weights] = feval(['mydata_' speciesName]);
+    % Get par struct
+    resultsMatFileName = ['results_' speciesName '.mat'];
+    parsInitFileName = ['pars_init_' speciesName '.m'];
+    if exist(resultsMatFileName, 'file')
+        load(resultsMatFileName, "par")
+    elseif exist(parsInitFileName, 'file')
+        % If results.mat file does not exist load parameters from
+        % pars_init.m file. This has lower precision due to rounding errors
+        % when printing to pars_init.m
+        [par, ~, ~] = feval(['pars_init_' speciesName], metaData);
+    end
+
+    % Run predict function
+    [prdData, info] = feval(['predict_' speciesName], par, data, auxData);
+    % Check if there was a failed predict filter
+    if ~info
+        debLoss = nan;
+        success = false;
+        errorType = "predict_filter";
+    else
+        % Compute loss
+        debLoss = deb_lossfun(data, prdData, weights, par, includePseudoData);
+        success = true;
+        errorType = "";
+    end
+
+else
+    error('Folder for species "%s" does not exist.', speciesName);
+end
+end
+
+% end
