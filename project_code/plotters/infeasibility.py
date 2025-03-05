0128c080fec0d8dbc65293290f8383fac344d6e4
Diogo Oliveira <diogo.oliveira898@gmail.com>
2025-03-05 15:05:53 +0000

    Code to compare feasibility results

---
+++ project_code/plotters/infeasibility.py
@@ -0,0 +1,12 @@
+infeasibility_flag_to_text = {
+    '0': 'Feasible',
+    '1': 'Non-positive \nparameters',
+    '2': 'Efficiency not \n between 0 and 1',
+    '3': 'Growth effieciency\n larger than 1',
+    '4': 'Maturities are not\n increasing',
+    '5': 'Puberty cannot \nbe reached',
+    '6': 'Birth cannot \nbe reached',
+    'error': 'Error',
+    'failed': 'Method failed',
+    'timeout': 'Time limit reached'
+}
