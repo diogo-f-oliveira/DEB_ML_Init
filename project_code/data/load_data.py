836dbb243f8af251c87a3d2510a2de95f4038c1a
Diogo Oliveira <diogo.oliveira898@gmail.com>
2025-03-12 23:17:47 +0000

    Datasets now define mask columns for output imputation
    New dataset files that include masks for output computation
    New structure for types_of_col
    col_types now includes a 'mask' class
    load_data renamed to load_dataframes´-> now returns dataframes and col_types

--- project_code/data/load_data.py
+++ project_code/data/load_data.py
@@ -2,14 +2,14 @@ import pandas as pd
 import json
 
 
-def load_data(dataset_name, datasets_folder='data/processed', data_split='train_val_test'):
+def load_dataframes(dataset_name, datasets_folder='data/processed', data_split='train_val_test'):
     col_types = load_col_types(dataset_name=dataset_name, datasets_folder=datasets_folder)
     data = {
         'train': pd.read_csv(f"{datasets_folder}/{dataset_name}/train.csv", index_col=0),
         'val': pd.read_csv(f"{datasets_folder}/{dataset_name}/val.csv", index_col=0),
         'test': pd.read_csv(f"{datasets_folder}/{dataset_name}/test.csv", index_col=0)
     }
-    ordered_cols = col_types['input']['all'] + col_types['output']['all']
+    ordered_cols = list(set(col_types['input']['all'] + col_types['output']['all'] + col_types['mask']['all']))
     for split, df in data.items():
         data[split] = df[ordered_cols]
     if data_split == 'train_test':
@@ -17,20 +17,24 @@ def load_data(dataset_name, datasets_folder='data/processed', data_split='train_
             'train': pd.concat([data['train'], data['val']], axis=0),
             'test': data['test'],
         }
-    return data
+    return data, col_types
 
 
 def load_col_types(dataset_name, datasets_folder='data/processed'):
-    col_types_options = ['all', 'boolean', 'scale', 'log', 'bounded01', 'quantile', 'category']
+    col_types_qualifiers = ['all', 'boolean', 'scale', 'log', 'bounded01', 'quantile', 'category']
 
     with open(f"{datasets_folder}/{dataset_name}/types_of_col.json", "r") as json_file:
         types_of_col = json.load(json_file)
-        col_types = {'input': {t: [] for t in col_types_options}, 'output': {t: [] for t in col_types_options}}
-        for col, type_list in types_of_col.items():
-            io = type_list[0]
-            for t in type_list:
-                if t == io:
-                    t = 'all'
-                col_types[io][t].append(col)
+        col_types = {
+            'input': {t: [] for t in col_types_qualifiers},
+            'output': {t: [] for t in col_types_qualifiers},
+            'mask': {t: [] for t in col_types_qualifiers},
+        }
+        for col, type_info in types_of_col.items():
+            for c in type_info['classes']:
+                col_types[c]['all'].append(col)
+            for t in type_info['qualifiers']:
+                for c in type_info['classes']:
+                    col_types[c][t].append(col)
 
     return col_types
